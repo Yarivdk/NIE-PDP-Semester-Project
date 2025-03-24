1b5c54158887695440c47396dc87064906b67ad2
výuka <vandeyar@cf-frontend03-prod.cls.in.fit.cvut.cz>
2025-03-24 09:28:06 +0100

    Changed the script.

--- script.sh
+++ script.sh
@@ -7,9 +7,6 @@
 #SBATCH --output="%x-%J.out"
 #SBATCH --error="%x-%J.err"
 
-# Retrieve assigned CPUs
-NUM_CPUS=${SLURM_CPUS_PER_TASK}
-
 # Get additional command-line arguments
 ARG1=$1
 ARG2=$2
@@ -22,6 +19,6 @@ source /etc/profile.d/zz-cray-pe.sh
 module load cray-mvapich2_pmix_nogpu
 
 # After the srun command, write the path to your program and its arguments for running on the scheduled computing nodes:
-srun ./output ${ARG1} ${ARG2} ${NUM_CPUS}
+srun ./output ${ARG1} ${ARG2}
 
 exit 0
