2fe7649832a8031cfe2e8bc25d9ce134bcec34e2
výuka <vandeyar@cf-frontend03-prod.cls.in.fit.cvut.cz>
2025-03-24 09:23:46 +0100

    Added a script to run the sequential solution on different types of nodes.

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Setting the control parameters of the Slurm scheduler
+#SBATCH --job-name=checkpoint1
+#SBATCH --nodes=1
+
+#SBATCH --output="%x-%J.out"
+#SBATCH --error="%x-%J.err"
+
+# Retrieve assigned CPUs
+NUM_CPUS=${SLURM_CPUS_PER_TASK}
+
+# Get additional command-line arguments
+ARG1=$1
+ARG2=$2
+
+# Activation of HPE CPE
+source /etc/profile.d/zz-cray-pe.sh
+
+# Setting environment variables for the scheduled task
+#module load cray-mvapich2_pmix_nogpu/2.3.7
+module load cray-mvapich2_pmix_nogpu
+
+# After the srun command, write the path to your program and its arguments for running on the scheduled computing nodes:
+srun ./output ${ARG1} ${ARG2} ${NUM_CPUS}
+
+exit 0
